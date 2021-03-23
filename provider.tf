b6899aa7d848aa17216845036b233a0fb97882a2
arienvicky <arienvicky@gmail.com>
2021-03-24 01:24:29 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,19 +1,3 @@
-/*
-terraform {
- required_providers {
- aws = {
-   region = "us-east-2"
-   }
- azurerm = {
- region = "us-east2"
- }
-}yes
-}
-*/
 provider "aws" {
-  region = "us-west-2"
-}
-provider "aws" {
-  alias  = "california"
   region = "us-west-2"
 }
